c74acbb2add15f72da23054c9af1aa0eb40d794f
Md Mehadi Sarkar <148835247+nanuvai017@users.noreply.github.com>
2023-11-19 16:20:37 +0600

    Update Mehadi.py

--- Mehadi.py
+++ Mehadi.py
@@ -1323,7 +1323,7 @@ def superuser():
         input(" Press Enter To Send Key")
         time.sleep(3.5)
         tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+UMO+ttt+id
-        os.system('am start https://wa.me/+8801303637752?text=' + tks)
+        os.system('am start https://wa.me/+8801606777866?text=' + tks)
         superuser()        
 superuser()
 
